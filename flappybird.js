714f09e2ebb9f0209c4ce41bd501370610cdd1ec
Harshsagar175 <121607176+Harshsagar175@users.noreply.github.com>
2023-08-11 02:11:16 +0530

    Update flappybird.js

--- flappybird.js
+++ flappybird.js
@@ -159,5 +159,5 @@ function moveBird(e){
 }
 
 function detectCollision(a , b){
-    return a.x < b.x && a.x + a.width > b.x && a.y < b.y + b.height && a.y + a.height > b.y;
+    return a.x < b.x + b.width && a.x + a.width > b.x && a.y < b.y + b.height && a.y + a.height > b.y;
 }
